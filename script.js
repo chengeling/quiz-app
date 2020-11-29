c5ba45ca2457d74a70d9d3c3be2b5b202fb57470
chengeling <przemek103@op.pl>
2020-11-29 15:52:36 +0100

    Implemented logic

--- script.js
+++ script.js
@@ -4,27 +4,63 @@ const container = document.querySelector('.container');
 const title = document.querySelector('.welcome-text');
 const startBtn = document.querySelector('.start-quiz-btn');
 const questionBox = document.querySelector('.question-box-hide');
-const nextQuestion = document.querySelector('.next-question-btn');
-const prevQuestion = document.querySelector('.prev-question-btn');
 
-
-function startQuiz() {
+//Start quiz
+startBtn.addEventListener('click', () => {
     title.classList.add('welcome-text-hide');
     startBtn.classList.add('start-quiz-hide-btn');
     setTimeout(() => questionBox.classList.add('question-box-show'), 600);
-}
+})
 
-function getQuestions() {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'questions.json');
-    xhr.responseType = 'json';
-    xhr.send();
-    xhr.addEventListener('load', () => {
-        return xhr.response.questions;
+//Główny program
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'questions.json');
+xhr.responseType = 'json';
+xhr.send();
+xhr.addEventListener('load', () => {
+    const title = xhr.response.title;
+    const questions = xhr.response.questions;
+    const quizTitle = document.createElement('h1');
+    quizTitle.classList.add('quiz-title')
+    quizTitle.textContent = title;
+    questionBox.appendChild(quizTitle);
+
+    // Generowanie pytań i inputów
+    for(let i = 1; i < questions.length + 1; i++) {
+        let { q, a } = questions[i - 1];
+        let questionText = document.createElement('h3');
+        questionText.classList.add(`question-text${i}`);
+        questionText.textContent = q
+        questionBox.appendChild(questionText);
+        let answerInput = document.createElement('input');
+        answerInput.classList.add(`answer-input${i}`)
+        questionBox.appendChild(answerInput);
+    }
+    
+    let checkAnswersBtn = document.createElement('button');
+    checkAnswersBtn.classList.add('check-answer-btn')
+    checkAnswersBtn.textContent = 'Sprawdź odpowiedzi!';
+    questionBox.appendChild(checkAnswersBtn);
+
+    //Sprawdzanie odpowiedzi
+    checkAnswersBtn.addEventListener('click', () => {
+        let score = 0;
+        for(let i = 1; i < questions.length + 1; i++) {
+            let userAnswer = document.querySelector(`.answer-input${i}`)
+            let { q: question, a: goodAnswer } = questions[i - 1];
+            if(userAnswer.value.toLowerCase() === goodAnswer.toLowerCase()) {
+                score+=1;
+                userAnswer.style.borderColor = 'rgb(39, 233, 39)';
+            } else {
+                userAnswer.style.borderColor = 'rgb(199, 38, 78)';
+            }
+        }
+        if(score !== questions.length) {
+            container.style.backgroundColor = 'rgb(199, 38, 78)';
+            setTimeout(() => alert(`Twój wynik to ${score}/${questions.length}. Spróbuj poprawić swoje odpowiedzi!`), 500)
+        } else {
+            container.style.backgroundColor = 'rgb(255, 174, 0)';
+            setTimeout(() => alert(`Twój wynik to ${score}/${questions.length}. Gratulacje!`), 500)
+        }
     })
-}
-
-startBtn.addEventListener('click', startQuiz)
-
-let data = getQuestions();
-console.log(data)
+});
